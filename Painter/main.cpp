d2eb076c456d7a383b5e81d193a244eaaad5a749
Nicolas Mellado <nmellado0@gmail.com>
2020-12-08 11:25:59 +0100

    Helper class, first attemps

--- Painter/main.cpp
+++ Painter/main.cpp
@@ -7,52 +7,90 @@
 #include <Engine/Component/GeometryComponent.hpp>
 #include <Engine/Managers/EntityManager/EntityManager.hpp>
 #include <Engine/System/GeometrySystem.hpp>
-#include <Engine/Renderer/RenderTechnique/ShaderConfiguration.hpp> 
-#include <Engine/Renderer/RenderTechnique/ShaderConfigFactory.hpp>
-#include <Engine/Renderer/RenderObject/RenderObject.hpp>
-#include <Engine/Renderer/RenderObject/RenderObjectManager.hpp>
-#include <Engine/Component/GeometryComponent.hpp>
+
+#include <Core/Utils/Color.hpp>
 
 #include "material.hpp"
+
+
+// 1. Implement a parameter provider to provide the uniforms for the shader
+class MyParameterProvider : public Ra::Engine::ShaderParameterProvider {
+public:
+  MyParameterProvider() {}
+  ~MyParameterProvider() {}
+  void updateGL() override {
+    // Method called before drawing each frame in Renderer::updateRenderObjectsInternal.
+    // The name of the parameter corresponds to the shader's uniform name.
+    m_renderParameters.addParameter( "aColorUniform", m_colorParameter );
+    m_renderParameters.addParameter( "aScalarUniform", m_scalarParameter );
+  }
+  void setOrComputeTheParameterValues() {
+    // client side computation of the parameters, e.g.
+    m_colorParameter = Ra::Core::Utils::Color::Red();
+    m_scalarParameter = .5_ra;
+  }
+private:
+  Ra::Core::Utils::Color m_colorParameter;
+  Scalar m_scalarParameter;
+};
+
+
+// 2. Implement a specific vertex and fragment shaders to compute the fragment color based on uniform values
+// Vertex shader source code
+const std::string vertex{
+    "#include \"TransformStructs.glsl\"\n"
+    "layout (location = 0) in vec3 in_position;\n"
+    "layout (location = 0) out vec3 out_pos;\n"
+    "uniform Transform transform;\n"
+    "void main(void)\n"
+    "{\n"
+    "    mat4 mvp    = transform.proj * transform.view;\n"
+    "    out_pos     = in_position;\n"
+    "    gl_Position = mvp*vec4(in_position.xyz, 1.0);\n"
+    "}\n"};
+// Fragment shader source code
+const std::string fragment{
+    "layout (location = 0) in  vec3 in_pos;\n"
+    "layout (location = 0) out vec4 out_color;\n"
+     "uniform vec4 aColorUniform;\n"
+     "uniform float aScalarUniform;\n"
+    "void main(void)\n"
+    "{\n"
+    "    out_color =  vec4(in_pos,1.)*aScalarUniform;\n"
+    "}\n"};
+
 
 void
 initQuad(Ra::GuiBase::BaseApplication &app)
 {
 
-        //! [Creating the cube]
+    //! [Creating the quad]
     auto cube = Ra::Core::Geometry::makeZNormalQuad( {1_ra, 1_ra} );
-    //! [Creating the cube]
 
     //! [Create the engine entity for the cube]
-    auto e = app.m_engine->getEntityManager()->createEntity( "Green cube" );
-    //! [Create the engine entity for the cube]
+    auto e = app.m_engine->getEntityManager()->createEntity( "Quad Entity" );
 
-    //! [Create a geometry component with the cube]
-    auto c = new Ra::Engine::TriangleMeshComponent( "Cube Mesh", e, std::move( cube ), nullptr );
-    //! [Create a geometry component with the cube]
+    //! [Create Parameter provider for the shader]
+    auto paramProvider = std::make_shared<MyParameterProvider>();
+    paramProvider->setOrComputeTheParameterValues();
+
+    //! [Create a geometry component]
+    auto c = new Ra::Engine::TriangleMeshComponent( "Quad Mesh", e, std::move( cube ), nullptr );
+
+    //! [Create a CustomShaderComponent]
+    auto c2 = new CustomShaderComponent( "Quad Mesh Shader", e, c, paramProvider, vertex, fragment);
 
     //! [Register the entity/component association to the geometry system ]
-    auto geometrySystem = app.m_engine->getSystem( "GeometrySystem" );
-    geometrySystem->addComponent( e, c );
-    //! [Register the entity/component association to the geometry system ]
+    auto system = app.m_engine->getSystem( "GeometrySystem" );
+    system->addComponent( e, c );
+    system->addComponent( e, c2 );
 
-    // 3. Setup a Ra::Engine::ShaderConfiguration that uses these shaders
-    Ra::Engine::ShaderConfiguration myConfig{"MyColorComputation"};
-    myConfig.addShaderSource( Ra::Engine::ShaderType::ShaderType_VERTEX, vertexShaderSource );
-    myConfig.addShaderSource( Ra::Engine::ShaderType::ShaderType_FRAGMENT, fragmentShaderSource );
-    Ra::Engine::ShaderConfigurationFactory::addConfiguration( myConfig );
-    // 4. Build a render technique that use this configuration
-    auto renderTechnique = Ra::Core::make_shared<Ra::Engine::RenderTechnique>();
-    renderTechnique->setConfiguration( myConfig, Ra::Engine::DefaultRenderingPasses::LIGHTING_OPAQUE );
-    // 5. Create and associate the parameter provider with the RenderTechnique
-    auto parameterProvider = std::make_shared<MyParameterProvider>();
-    parameterProvider->setOrComputeTheParameterValues();
-    renderTechnique->setParametersProvider(parameterProvider);
-    // 6. Associate the render technique with a geometry in a Ra::Engine::RenderObject
+    // Initialize is not call automatically, why ? 
+    // What is the purpose of this function if it is not used ?
+    c2->initialize();
 
-    auto ro    = Ra::Engine::RadiumEngine::getInstance()->getRenderObjectManager()->getRenderObject(
-        c->m_renderObjects[0]);
-    ro->setRenderTechnique( renderTechnique );
+    // Calling this function remove the custom material, I am not sure to understand why
+    //app.m_mainWindow->postLoadFile( "Cube" );
 }
 
 int main( int argc, char* argv[] ) {
--- Painter/material.hpp
+++ Painter/material.hpp
@@ -1,50 +1,62 @@
 #pragma once
 
+#include <string>
+
+#include <Engine/Component/GeometryComponent.hpp>
 #include <Engine/Renderer/RenderTechnique/RenderParameters.hpp>
-#include <Core/Utils/Color.hpp>
+#include <Engine/Renderer/RenderTechnique/ShaderConfiguration.hpp> 
+#include <Engine/Renderer/RenderTechnique/ShaderConfigFactory.hpp>
+#include <Engine/Renderer/RenderObject/RenderObject.hpp>
+#include <Engine/Renderer/RenderObject/RenderObjectManager.hpp>
 
 
-// 1. Implement a parameter provider to provide the uniforms for the shader
-class MyParameterProvider : public Ra::Engine::ShaderParameterProvider {
-public:
-  MyParameterProvider() {}
-  ~MyParameterProvider() {}
-  void updateGL() override {
-    // Method called before drawing each frame in Renderer::updateRenderObjectsInternal.
-    // The name of the parameter corresponds to the shader's uniform name.
-    m_renderParameters.addParameter( "aColorUniform", m_colorParameter );
-    m_renderParameters.addParameter( "aScalarUniform", m_scalarParameter );
-  }
-  void setOrComputeTheParameterValues() {
-    // client side computation of the parameters, e.g.
-    m_colorParameter = Ra::Core::Utils::Color::Red();
-    m_scalarParameter = .5_ra;
-  }
-private:
-  Ra::Core::Utils::Color m_colorParameter;
-  Scalar m_scalarParameter;
+
+/**
+ * Helper component allowing to add a custom shader to an existing object.
+ * 
+ * The custom object must already habve a GeometryComponent to get access to the RenderObject.
+ * The material is set when calling initialize.
+ */
+class CustomShaderComponent : public Ra::Engine::Component
+{
+  public:
+    inline CustomShaderComponent( const std::string& name, 
+                           Ra::Engine::Entity* entity,
+
+                           Ra::Engine::GeometryComponent* geoComp,
+
+                           std::shared_ptr<Ra::Engine::ShaderParameterProvider> parameterProvider,
+                           const std::string& vertexShaderSource,
+                           const std::string& fragmentShaderSource) :
+        Ra::Engine::Component( name, entity ), 
+        _geoComp(geoComp),
+        _parameterProvider(parameterProvider), 
+        _vertexShaderSource(vertexShaderSource),
+        _fragmentShaderSource(fragmentShaderSource) { }
+    ~CustomShaderComponent() override {}
+
+    inline void initialize() override {
+        // 3. Setup a Ra::Engine::ShaderConfiguration that uses these shaders
+        Ra::Engine::ShaderConfiguration myConfig{getName()};
+        myConfig.addShaderSource( Ra::Engine::ShaderType::ShaderType_VERTEX, _vertexShaderSource );
+        myConfig.addShaderSource( Ra::Engine::ShaderType::ShaderType_FRAGMENT, _fragmentShaderSource );
+        Ra::Engine::ShaderConfigurationFactory::addConfiguration( myConfig );
+
+        // 4. Build a render technique that use this configuration
+        auto renderTechnique = Ra::Core::make_shared<Ra::Engine::RenderTechnique>();
+        renderTechnique->setConfiguration( myConfig, Ra::Engine::DefaultRenderingPasses::LIGHTING_OPAQUE );
+
+        // 5. Create and associate the parameter provider with the RenderTechnique
+        renderTechnique->setParametersProvider(_parameterProvider);
+
+        // 6. Associate the render technique with a geometry in a Ra::Engine::RenderObject
+        auto ro    = Ra::Engine::RadiumEngine::getInstance()->getRenderObjectManager()->getRenderObject(
+            _geoComp->m_renderObjects[0]);
+        ro->setRenderTechnique( renderTechnique );
+    }
+
+    private:
+        Ra::Engine::GeometryComponent* _geoComp;
+        std::shared_ptr<Ra::Engine::ShaderParameterProvider> _parameterProvider;
+        const std::string& _vertexShaderSource, _fragmentShaderSource;
 };
-
-// 2. Implement a specific vertex and fragment shaders to compute the fragment color based on uniform values
-// Vertex shader source code
-const std::string vertexShaderSource{
-    "#include \"TransformStructs.glsl\"\n"
-    "layout (location = 0) in vec3 in_position;\n"
-    "layout (location = 0) out vec3 out_pos;\n"
-    "uniform Transform transform;\n"
-    "void main(void)\n"
-    "{\n"
-    "    mat4 mvp    = transform.proj * transform.view;\n"
-    "    out_pos     = in_position;\n"
-    "    gl_Position = mvp*vec4(in_position.xyz, 1.0);\n"
-    "}\n"};
-// Fragment shader source code
-const std::string fragmentShaderSource{
-    "layout (location = 0) in  vec3 in_pos;\n"
-    "layout (location = 0) out vec4 out_color;\n"
-     "uniform vec4 aColorUniform;\n"
-     "uniform float aScalarUniform;\n"
-    "void main(void)\n"
-    "{\n"
-    "    out_color =  vec4(in_pos,1.)*aScalarUniform;\n"
-    "}\n"};

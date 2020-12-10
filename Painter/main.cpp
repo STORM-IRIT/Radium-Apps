c03876e20398785c60d6293f99432ebaff126bb3
Nicolas Mellado <nmellado0@gmail.com>
2020-12-10 16:26:43 +0100

    Update code according to latest demo in master

--- Painter/main.cpp
+++ Painter/main.cpp
@@ -6,111 +6,132 @@
 #include <Core/Geometry/MeshPrimitives.hpp>
 #include <Engine/Component/GeometryComponent.hpp>
 #include <Engine/Managers/EntityManager/EntityManager.hpp>
+#include <Engine/Renderer/RenderObject/RenderObjectManager.hpp>
 #include <Engine/System/GeometrySystem.hpp>
 
-#include <Core/Utils/Color.hpp>
+// include the custom material definition
+#include <Engine/Renderer/Material/RawShaderMaterial.hpp>
 
-#include "material.hpp"
+// include the Viewer to demonstrate dynamic edition of materials
+#include <GuiBase/Viewer/Viewer.hpp>
 
+#include "CameraManipulator.hpp"
 
-// 1. Implement a parameter provider to provide the uniforms for the shader
-class MyParameterProvider : public Ra::Engine::ShaderParameterProvider {
-public:
-  MyParameterProvider() {}
-  ~MyParameterProvider() {}
-  void updateGL() override {
-    // Method called before drawing each frame in Renderer::updateRenderObjectsInternal.
-    // The name of the parameter corresponds to the shader's uniform name.
-    m_renderParameters.addParameter( "aColorUniform", m_colorParameter );
-    m_renderParameters.addParameter( "aScalarUniform", m_scalarParameter );
-  }
-  void setOrComputeTheParameterValues() {
-    // client side computation of the parameters, e.g.
-    m_colorParameter = Ra::Core::Utils::Color::Red();
-    m_scalarParameter = .5_ra;
-  }
-private:
-  Ra::Core::Utils::Color m_colorParameter;
-  Scalar m_scalarParameter;
-};
+// Qt
+#include <QTimer>
 
-
-// 2. Implement a specific vertex and fragment shaders to compute the fragment color based on uniform values
+/**
+ * Demonstrate the usage of RawShaderMaterial functionalities
+ */
 // Vertex shader source code
-const std::string vertex{
-    "#include \"TransformStructs.glsl\"\n"
-    "layout (location = 0) in vec3 in_position;\n"
-    "layout (location = 0) out vec3 out_pos;\n"
-    "uniform Transform transform;\n"
-    "void main(void)\n"
-    "{\n"
-    "    mat4 mvp    = transform.proj * transform.view;\n"
-    "    out_pos     = in_position;\n"
-    "    gl_Position = mvp*vec4(in_position.xyz, 1.0);\n"
-    "}\n"};
+const std::string _vertexShaderSource {"#include \"TransformStructs.glsl\"\n"
+                                       "layout (location = 0) in vec3 in_position;\n"
+                                       "layout (location = 0) out vec3 out_pos;\n"
+                                       "uniform Transform transform;\n"
+                                       "void main(void)\n"
+                                       "{\n"
+                                       "    mat4 mvp    = transform.proj * transform.view;\n"
+                                       "    out_pos     = in_position;\n"
+                                       "    gl_Position = mvp*vec4(in_position.xyz, 1.0);\n"
+                                       "}\n"};
 // Fragment shader source code
-const std::string fragment{
+const std::string _fragmentShaderSource {
     "layout (location = 0) in  vec3 in_pos;\n"
     "layout (location = 0) out vec4 out_color;\n"
-     "uniform vec4 aColorUniform;\n"
-     "uniform float aScalarUniform;\n"
+    "uniform vec4 aColorUniform;\n"
+    "uniform float aScalarUniform;\n"
     "void main(void)\n"
     "{\n"
-    "    out_color =  vec4(in_pos,1.)*aScalarUniform;\n"
+    "    out_color =  ( 1 + cos( 20 * ( in_pos.x + aScalarUniform ) ) ) * 0.5 * aColorUniform;\n"
     "}\n"};
+
 // Fragment shader source code
-const std::string fragment2{
+const std::string _fragmentShaderSource2 {
+    "layout (location = 0) in  vec3 in_pos;\n"
     "layout (location = 0) out vec4 out_color;\n"
-     "uniform vec4 aColorUniform;\n"
-     "uniform float aScalarUniform;\n"
+    "uniform vec4 aColorUniform;\n"
+    "uniform float aScalarUniform;\n"
     "void main(void)\n"
     "{\n"
-    "    out_color =  aColorUniform*aScalarUniform;\n"
+    "    out_color =  ( 1 + sin( 20 * ( in_pos.y + aScalarUniform ) ) ) * 0.5 * aColorUniform;\n"
     "}\n"};
 
+const std::vector<std::pair<Ra::Engine::ShaderType, std::string>> _config1 {
+    {Ra::Engine::ShaderType::ShaderType_VERTEX, _vertexShaderSource},
+    {Ra::Engine::ShaderType::ShaderType_FRAGMENT, _fragmentShaderSource}};
 
-void
-initQuad(Ra::GuiBase::BaseApplication &app, 
-        Scalar size,
-        std::shared_ptr<Ra::Engine::ShaderParameterProvider> parameterProvider,
-        std::string vert, std::string frag)
+const std::vector<std::pair<Ra::Engine::ShaderType, std::string>> _config2 {
+    {Ra::Engine::ShaderType::ShaderType_VERTEX, _vertexShaderSource},
+    {Ra::Engine::ShaderType::ShaderType_FRAGMENT, _fragmentShaderSource2}};
+
+class MyParameterProvider : public Ra::Engine::ShaderParameterProvider
 {
+  public:
+    MyParameterProvider() {}
+    ~MyParameterProvider() {}
+    void updateGL() override {
+        // Method called before drawing each frame in Renderer::updateRenderObjectsInternal.
+        // The name of the parameter corresponds to the shader's uniform name.
+        m_renderParameters.addParameter( "aColorUniform", m_colorParameter );
+        m_renderParameters.addParameter( "aScalarUniform", m_scalarParameter );
+    }
+    void setOrComputeTheParameterValues() {
+        // client side computation of the parameters, e.g.
+        m_colorParameter  = Ra::Core::Utils::Color::Red();
+        m_scalarParameter = .5_ra;
+    }
 
+  private:
+    Ra::Core::Utils::Color m_colorParameter {Ra::Core::Utils::Color::Green()};
+    Scalar m_scalarParameter {1};
+};
+
+/**
+ * Generate a quad with a ShaderMaterial attached
+ * @param app
+ * @return The renderObject associated to the created component.
+ */
+std::shared_ptr<Ra::Engine::RenderObject> initQuad( Ra::GuiBase::BaseApplication& app ) {
     //! [Creating the quad]
-    auto cube = Ra::Core::Geometry::makeZNormalQuad( {size, size} );
+    auto quad = Ra::Core::Geometry::makeZNormalQuad( {1_ra, 1_ra} );
 
-    //! [Create the engine entity for the cube]
+    //! [Create the engine entity for the quad]
     auto e = app.m_engine->getEntityManager()->createEntity( "Quad Entity" );
-
-    //! [Create a geometry component]
-    auto c = new Ra::Engine::TriangleMeshComponent( "Quad Mesh", e, std::move( cube ), nullptr );
-
-    //! [Create a CustomShaderComponent]
-    auto c2 = new CustomShaderComponent( "Quad Mesh Shader", e, c, parameterProvider, vert, frag);
-
-    //! [Register the entity/component association to the geometry system ]
-    auto system = app.m_engine->getSystem( "GeometrySystem" );
-    system->addComponent( e, c );
-    system->addComponent( e, c2 );
-
-    // Initialize is not call automatically, why ? 
-    // What is the purpose of this function if it is not used ?
-    c2->initialize();
-
-    // Calling this function remove the custom material, I am not sure to understand why
-    //app.m_mainWindow->postLoadFile( "Cube" );
-}
-
-int main( int argc, char* argv[] ) {
-    Ra::GuiBase::BaseApplication app( argc, argv, Ra::GuiBase::SimpleWindowFactory {} );
-
 
     //! [Create Parameter provider for the shader]
     auto paramProvider = std::make_shared<MyParameterProvider>();
     paramProvider->setOrComputeTheParameterValues();
 
-    initQuad( app, 0.5, paramProvider, vertex, fragment );
-    initQuad( app, 1., paramProvider, vertex, fragment2 );
+    //! [Create the shader material]
+    Ra::Core::Asset::RawShaderMaterialData mat {"Quad Material", _config1, paramProvider};
+
+    //! [Create a geometry component using the custom material]
+    auto c = new Ra::Engine::TriangleMeshComponent( "Quad Mesh", e, std::move( quad ), &mat );
+
+    //! [Register the entity/component association to the geometry system ]
+    auto system = app.m_engine->getSystem( "GeometrySystem" );
+    system->addComponent( e, c );
+
+    //![get the renderobject for further edition]
+    auto ro = Ra::Engine::RadiumEngine::getInstance()->getRenderObjectManager()->getRenderObject(
+        c->m_renderObjects[0] );
+
+    // Initialize all OpenGL state for the scene content
+    app.m_mainWindow->postLoadFile( "Cube" );
+    return ro;
+}
+
+int main( int argc, char* argv[] ) {
+    Ra::GuiBase::BaseApplication app( argc, argv, Ra::GuiBase::SimpleWindowFactory {} );
+
+    //! [add the custom material to the material system]
+    Ra::Engine::RawShaderMaterial::registerMaterial();
+
+    auto ro = initQuad( app );
+
+    auto viewer = app.m_mainWindow->getViewer();
+    viewer->setCameraManipulator(
+        new CameraManipulator2D( *( viewer->getCameraManipulator() ) ) );
 
     return app.exec();
 }

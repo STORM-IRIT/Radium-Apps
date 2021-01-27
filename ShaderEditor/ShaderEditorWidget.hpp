a77f6a5ed7eec6f1aebd605c026f11075b20ab65
Nicolas Mellado <nmellado0@gmail.com>
2021-01-27 11:35:53 +0100

    Update material from UI

--- ShaderEditor/ShaderEditorWidget.hpp
+++ ShaderEditor/ShaderEditorWidget.hpp
@@ -1,5 +1,4 @@
-#ifndef SHADEREDITORWIDGET_H
-#define SHADEREDITORWIDGET_H
+#pragma once
 
 #include <QWidget>
 
@@ -7,16 +6,36 @@ namespace Ui {
 class ShaderEditorWidget;
 }
 
+namespace Ra{
+namespace Engine{
+    class RenderObject;
+    class Renderer;
+    class ShaderParameterProvider;
+}
+}
+
+///
+/// \todo Add automatic UI generation from parameter provider
 class ShaderEditorWidget : public QWidget
 {
     Q_OBJECT
 
 public:
-    explicit ShaderEditorWidget(QWidget *parent = nullptr);
+    explicit ShaderEditorWidget(const std::string& v,
+                                const std::string& f,
+                                std::shared_ptr< Ra::Engine::RenderObject > ro,
+                                Ra::Engine::Renderer * renderer,
+                                std::shared_ptr< Ra::Engine::ShaderParameterProvider > paramProvider,
+                                QWidget *parent = nullptr);
     ~ShaderEditorWidget();
+
+private slots:
+    void updateShadersFromUI();
 
 private:
     Ui::ShaderEditorWidget *ui;
+    std::shared_ptr< Ra::Engine::RenderObject > _ro;
+    Ra::Engine::Renderer * _renderer;
+    std::shared_ptr< Ra::Engine::ShaderParameterProvider > _paramProvider;
 };
 
-#endif // SHADEREDITORWIDGET_H
